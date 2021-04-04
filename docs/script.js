e05131baa898b600ec4e6880b8fc3028292edea2
dhayesboi <idhaaboi@gmail.com>
2021-04-04 16:22:17 +0800

    fixes hopefully
    huh

--- docs/script.js
+++ docs/script.js
@@ -1,10 +1,18 @@
 function changeNav(){
-  if document.getElementById("mySidenav").stye.width == "220px"{
-    document.getElementById("mySidenav").style.width = "0";
-    document.getElementById("main").style.marginLeft= "0";
-  } else {
+  var indiv = document.getElementById('invisible');
+  var div = document.getElementById('visible');
+
+  if (document.getElementById("mySidenav").style.width == "220px"){
+    div.classList.add('invisible')
+    div.classList.remove('visible')
+    document.getElementById("mySidenav").style.width = "55";
+    document.getElementById("main").style.marginLeft= "65";
+  }
+  if (document.getElementById("mySidenav").style.width == "35px"){
+    indiv.classList.add('visible')
+    indiv.classList.remove('invisible');
     document.getElementById("mySidenav").style.width = "220px";
-    document.getElementById("main").style.marginLeft = "240px";
+    document.getElementById("main").style.marginLeft = "230px";
   }
 }
 /* old and bad functions, no one likes these get if statements lOL.
