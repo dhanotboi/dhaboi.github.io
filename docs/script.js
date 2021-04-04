c1f7877101e28adc65b2590c41eb5ca1dd4ca0ba
dhayesboi <idhaaboi@gmail.com>
2021-04-04 15:48:33 +0800

    images
    added images and worked them too the side nav

--- docs/script.js
+++ docs/script.js
@@ -1,3 +1,13 @@
+function changeNav(){
+  if document.getElementById("mySidenav").stye.width == "220px"{
+    document.getElementById("mySidenav").style.width = "0";
+    document.getElementById("main").style.marginLeft= "0";
+  } else {
+    document.getElementById("mySidenav").style.width = "220px";
+    document.getElementById("main").style.marginLeft = "240px";
+  }
+}
+/* old and bad functions, no one likes these get if statements lOL.
 function openNav() {
   document.getElementById("mySidenav").style.width = "220px";
   document.getElementById("main").style.marginLeft = "240px";
@@ -7,3 +17,4 @@ function closeNav() {
   document.getElementById("mySidenav").style.width = "0";
   document.getElementById("main").style.marginLeft= "0";
 }
+*/
