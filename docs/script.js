677d0a6fa4ef0e292a982abece6cb4b8a70c3fc9
dhayesboi <idhaaboi@gmail.com>
2021-04-22 09:37:30 +0800

    fixed alot
    made sidebar work

--- docs/script.js
+++ docs/script.js
@@ -1,28 +1,19 @@
 function changeNav(){
-  var indiv = document.getElementById('invisible');
-  var div = document.getElementById('visible');
-
-  if (document.getElementById("mySidenav").style.width == "220px"){
-    div.classList.add('invisible')
-    div.classList.remove('visible')
-    document.getElementById("mySidenav").style.width = "55";
-    document.getElementById("main").style.marginLeft= "65";
-  }
-  if (document.getElementById("mySidenav").style.width == "35px"){
-    indiv.classList.add('visible')
-    indiv.classList.remove('invisible');
-    document.getElementById("mySidenav").style.width = "220px";
-    document.getElementById("main").style.marginLeft = "230px";
+  if (document.getElementById("mySidenav").style.width == "300px"){
+    document.getElementById("mySidenav").style.width = "65px";
+    document.getElementById("main").style.marginLeft = "75px";
+  } else {
+    document.getElementById("mySidenav").style.width = "300px";
+    document.getElementById("main").style.marginLeft = "310px";
   }
 }
-/* old and bad functions, no one likes these get if statements lOL.
+
 function openNav() {
   document.getElementById("mySidenav").style.width = "220px";
   document.getElementById("main").style.marginLeft = "240px";
 }
 
 function closeNav() {
-  document.getElementById("mySidenav").style.width = "0";
-  document.getElementById("main").style.marginLeft= "0";
+  document.getElementById("mySidenav").style.width = "65px";
+  document.getElementById("main").style.marginLeft= "55px";
 }
-*/
