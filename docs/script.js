2309fdbe3e207c99d9dd822ff6f9d18327ead429
dhayesboi <idhaaboi@gmail.com>
2021-04-22 10:10:04 +0800

    made it all look good

--- docs/script.js
+++ docs/script.js
@@ -1,19 +1,9 @@
 function changeNav(){
   if (document.getElementById("mySidenav").style.width == "300px"){
-    document.getElementById("mySidenav").style.width = "65px";
-    document.getElementById("main").style.marginLeft = "75px";
+    document.getElementById("mySidenav").style.width = "75px";
+    document.getElementById("main").style.marginLeft = "85px";
   } else {
     document.getElementById("mySidenav").style.width = "300px";
     document.getElementById("main").style.marginLeft = "310px";
   }
-}
-
-function openNav() {
-  document.getElementById("mySidenav").style.width = "220px";
-  document.getElementById("main").style.marginLeft = "240px";
-}
-
-function closeNav() {
-  document.getElementById("mySidenav").style.width = "65px";
-  document.getElementById("main").style.marginLeft= "55px";
 }
