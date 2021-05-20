78ae98d3911081f8b9bcb0eec9887f2ad706e53d
dhayesboi <Shahd1@kgv.hk>
2021-05-20 09:45:15 +0800

    fixes

--- docs/script.js
+++ docs/script.js
@@ -1,11 +1,11 @@
 
 function changeNav(){
   if (document.getElementById("mySidenav").style.width == "300px"){
-    document.getElementById("mySidenav").style.width = "75px";
-    document.getElementById("content").style.margin = "10px 0px 0px 0px;";
+    document.getElementById("mySidenav").style.width = "85px";
+    document.getElementById("content").style.margin = "95px 0px 0px 0px;";
   } else {
     document.getElementById("mySidenav").style.width = "300px";
-    document.getElementById("content").style.margin = "10px 0px 0px 0px;";
+    document.getElementById("content").style.margin = "310px 0px 0px 0px;";
   }
   var invtext = document.getElementsByClassName("invisible");
   if (typeof(invtext.style.dispaly) != "undefined"){
@@ -21,12 +21,10 @@ function changeNav(){
 var slideIndex = 1;
 showSlides(slideIndex);
 
-// Next/previous controls
 function plusSlides(n) {
   showSlides(slideIndex += n);
 }
 
-// Thumbnail image controls
 function currentSlide(n) {
   showSlides(slideIndex = n);
 }
@@ -35,14 +33,14 @@ function showSlides(n) {
   var i;
   var slides = document.getElementsByClassName("mySlides");
   var dots = document.getElementsByClassName("dot");
-  if (n > slides.length) {slideIndex = 1}
+  if (n > slides.length) {slideIndex = 1}    
   if (n < 1) {slideIndex = slides.length}
   for (i = 0; i < slides.length; i++) {
-      slides[i].style.display = "none";
+      slides[i].style.display = "none";  
   }
   for (i = 0; i < dots.length; i++) {
       dots[i].className = dots[i].className.replace(" active", "");
   }
-  slides[slideIndex-1].style.display = "block";
+  slides[slideIndex-1].style.display = "block";  
   dots[slideIndex-1].className += " active";
 }
