f20dfee676475bd8efa5cd3095f317a22b8b1b07
dhayesboi <Shahd1@kgv.hk>
2021-05-26 20:40:45 +0800

    update to scroll bars
    + a card

--- docs/script.js
+++ docs/script.js
@@ -4,3 +4,12 @@ function openNav() {
 function closeNav() {
     document.getElementById("myNav").style.height = "0%";
 }
+for (var i = 0; i < elements.length; i++) {
+    elements[i].addEventListener('mouseover', function (e) {
+        document.getElementById('detail').style.display = "box";
+    })
+
+    elements[i].addEventListener('mouseout', function (e) {
+        document.getElementById('detail').style.display = "none";
+    })
+}
