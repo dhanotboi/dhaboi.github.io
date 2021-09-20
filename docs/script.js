cea595896c9c90eb4b3a1dbb9a4cfd6b4b5ba6fa
wylited <44154103+wylited@users.noreply.github.com>
2021-09-20 17:26:54 +0800

    Revert "init commit"
    This reverts commit 1c082071a1fe9517dc9f68ab72b461fb180e0ed5.

---
+++ docs/script.js
@@ -0,0 +1,10 @@
+function openNav() {
+    document.getElementById("myNav").style.height = "100%";
+}
+function closeNav() {
+    document.getElementById("myNav").style.height = "0%";
+}
+$(window).load(function() {
+	$(".loader").delay(2000).fadeOut("slow");
+  $("#overlayer").delay(2000).fadeOut("slow");
+})
