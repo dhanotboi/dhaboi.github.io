8f28d639263f04b489f176d16ff1f301bb13b6f4
wylited <44154103+wylited@users.noreply.github.com>
2021-09-20 21:00:58 +0800

    fade

--- docs/script.js
+++ docs/script.js
@@ -4,7 +4,12 @@ function openNav() {
 function closeNav() {
     document.getElementById("myNav").style.height = "0%";
 }
+
 $(window).load(function() {
 	$(".loader").delay(2000).fadeOut("slow");
   $("#overlayer").delay(2000).fadeOut("slow");
 })
+
+window.addEventListener("beforeunload", function () {
+  document.body.classList.add("animate-out");
+});
